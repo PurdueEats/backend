f33481f1fca45ac205726e4a569b0e9facfc39a3
Sean Joo <54560108+seanjoo4@users.noreply.github.com>
2021-02-25 18:08:15 -0500

    Create menuitemreviews.py

---
+++ backend/API/routes/menuitemreviews.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from mealratingmodel import MealRating
+
+app = FastAPI()
+
+data = []
+
+# get every meal rating
+@app.get("/MenuItemReviews")
+async def get_meal_ratings():
+  return data
+
+#get specific meal rating
+@app.get("/MenuItemReviews/{menu_id}")
+async def get_a_meal_rating(menu_id: int):
+  return data[menu_id - 1]
+
+#add meal rating
+@app.post("/MenuItemReviews")
+async def add_meal_rating(meal_rating: MealRating):
+  data.append(meal_rating.dict())
+  return data[-1]
+
+#update meal rating
+@app.put("/MenuItemReviews/{menu_id}")
+async def update_meal_rating(menu_id: int, meal_rating: MealRating):
+  data[menu_id - 1] = meal_rating.dict()
+  return data[menu_id - 1]
