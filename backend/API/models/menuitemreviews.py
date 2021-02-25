b4297c8d2a61c97070bb88807ce9137573eff57f
Sean Joo <54560108+seanjoo4@users.noreply.github.com>
2021-02-24 23:17:05 -0500

    Create menuitemreviews.py

---
+++ backend/API/models/menuitemreviews.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+data = []
+#separate file
+class MealRating(BaseModel):
+  meal_name: str
+  meal_id: int
+  # 0 to 5 scale
+  rating: int
+
+# get every meal rating
+@app.get("/MenuItemReviews")
+async def get_meal_ratings():
+  return data
+
+#get specific meal rating
+@app.get("/MenuItemReviews/{menu_id}")
+async def get_a_meal_rating(menu_id: int):
+  return data[menu_id - 1]
+
+#add meal rating
+@app.post("/MenuItemReviews")
+async def add_meal_rating(meal_rating: MealRating):
+  data.append(meal_rating.dict())
+  return data[-1]
+
+#update meal rating
+@app.put("/MenuItemReviews/{menu_id}")
+async def update_meal_rating(menu_id: int, meal_rating: MealRating):
+  data[menu_id - 1] = meal_rating.dict()
+  return data[menu_id - 1]
