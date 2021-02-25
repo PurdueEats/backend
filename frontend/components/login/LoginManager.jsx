4971949b8d1e58ec835c82fb5154b800a4c1d796
Aniket Agnihotri <aniket360@gmail.com>
2021-02-25 14:14:32 -0500

    login page base schema

--- frontend/components/login/LoginManager.jsx
+++ frontend/components/login/LoginManager.jsx
@@ -1,36 +1,106 @@
 import React from "react";
 import { Image, StyleSheet, View, Text } from "react-native";
 import Logo from "../../resources/logo.png";
-import { Button } from 'native-base';
+import { Button, Item, Input } from 'native-base';
 
 const LoginManager = () => {
-
     return (
-        <View style={ styles.baseText }>
-            <Image source={ Logo } style={ styles.image }/>
-            <Text style={ styles.titleText }>
-                PurdueEats
-            </Text>
-            <Button>
-                <Text>Click me!</Text>
-            </Button>
+        <View >
+            <View style={ styles.logoView }>
+                <Image source={ Logo } /*style={ styles.logo }*//>
+                <Text style={ styles.logoText }>
+                    PurdueEats
+                </Text>
+            </View>
+            <View style={ styles.formView }>
+                <Text style={ styles.formTitle }>Sign In</Text>
+                <Text style={ styles.componentIdentifier }>Email</Text>
+                <Item floatingLabel style={ styles.emailComponent }>
+                    <Input />
+                </Item>
+                <Text style={ styles.componentIdentifier }>Password</Text>
+                <Item floatingLabel style={ styles.passwordComponent }>
+                    <Input />
+                </Item>
+            </View>
+            <View style={ styles.actionView }>
+                <Button style={ styles.loginButtonComponent }>
+                    <Text style={ styles.loginButtonText }>Sign In</Text>
+                </Button>
+                <View style={ styles.endingText }>
+                    <Text style={ styles.forgotPasswordText }>Forgot Password?</Text>
+                    <Text style={ styles.signUpText }>Sign Up</Text>
+                </View>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    baseText: {
-        flex: 1,
-        paddingTop: 50,
+    logoView: {
+        paddingTop: "12%",
         alignItems: "center",
     },
-    image: {
-        width: 150,
-        height: 150,
-    },
-    titleText: {
-        fontSize: 30,
+    // logo: {
+    //     width: "40%",
+    //     height: "20%",
+    // },
+    logoText: {
+        marginBottom: "8%",
+        fontSize: 18,
         fontWeight: "bold"
+    },
+    formView: {
+        paddingTop: "2%",
+        paddingLeft: "10%",
+        paddingRight: "10%",
+        paddingBottom: "12%"
+    },
+    formTitle: {
+        fontSize: 25,
+        fontWeight: "bold",
+        marginBottom: "10%"
+    },
+    componentIdentifier: {
+        color: "red",
+        fontWeight: "bold"
+    },
+    emailComponent: {
+        marginBottom: "8%",
+        height: 40
+    },
+    passwordComponent: {
+        height: 40
+    },
+    actionView: {
+        paddingLeft: "10%",
+        paddingRight: "10%",
+    },
+    loginButtonComponent: {
+        width: '100%',
+        justifyContent: 'center',
+        backgroundColor: "red",
+        borderRadius: 10
+    },
+    loginButtonText: {
+        fontSize: 16,
+        fontWeight: "bold",
+        color: "white"
+    },
+    endingText: {
+        alignItems: "center"
+    },
+    forgotPasswordText: {
+        alignItems: "center",
+        paddingTop: "5%",
+        paddingBottom: "3%",
+        fontWeight: "bold",
+        color: "gray"
+    },
+    signUpText: {
+        alignItems: "center",
+        fontWeight: "bold",
+        color: "red"
     }
 });
 
