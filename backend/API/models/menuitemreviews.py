78eea9c235f6050fc7b0b0447e3feca667324b7f
Sean Joo <54560108+seanjoo4@users.noreply.github.com>
2021-02-24 23:21:46 -0500

    Update menuitemreviews.py

--- backend/API/models/menuitemreviews.py
+++ backend/API/models/menuitemreviews.py
@@ -4,30 +4,30 @@ from pydantic import BaseModel
 app = FastAPI()
 
 data = []
-#separate file
+
 class MealRating(BaseModel):
   meal_name: str
   meal_id: int
   # 0 to 5 scale
   rating: int
 
-# get every meal rating
+# get every meal rating from data
 @app.get("/MenuItemReviews")
 async def get_meal_ratings():
   return data
 
-#get specific meal rating
+#get specific meal rating from data
 @app.get("/MenuItemReviews/{menu_id}")
 async def get_a_meal_rating(menu_id: int):
   return data[menu_id - 1]
 
-#add meal rating
+#add meal rating to data
 @app.post("/MenuItemReviews")
 async def add_meal_rating(meal_rating: MealRating):
   data.append(meal_rating.dict())
   return data[-1]
 
-#update meal rating
+#update meal rating from data
 @app.put("/MenuItemReviews/{menu_id}")
 async def update_meal_rating(menu_id: int, meal_rating: MealRating):
   data[menu_id - 1] = meal_rating.dict()
