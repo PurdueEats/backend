6edd83001185a5a89058ed90d3378add6dd5a960
Sean Joo <54560108+seanjoo4@users.noreply.github.com>
2021-02-26 14:30:53 -0500

    Update menuitemreview.py

--- backend/API/models/menuitemreview.py
+++ backend/API/models/menuitemreview.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class MenuItemReview(BaseModel):
   user_id: int
   menu_item_id: int
   # 0 to 5 scale
-  rating: int
+  rating: int = Field(ge=0, le=5)
   timestamp: datetime
