a886eabb40975ea7539ad8d39571350adffb2fd0
Vaastav Arora <arora74@purdue.edu>
2021-02-26 23:09:03 -0500

    Module Fixes

--- backend/API/models/menuitemreview.py
+++ backend/API/models/menuitemreview.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel, Field
 from datetime import datetime
 
-class MealRating(BaseModel):
+#MenuItemReview Model
+class MenuItemReview(BaseModel):
   user_id: int
   menu_item_id: int
   # 0 to 5 scale
