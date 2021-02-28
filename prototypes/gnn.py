bc961223664804248b77f7b64c1b04b57bfc9070
Vaastav Arora <arora74@purdue.edu>
2021-02-27 19:14:43 -0500

    Base Layer added

--- prototypes/gnn.py
+++ prototypes/gnn.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
+from  GNN.Util import *
+
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
-
 # Layer IDs
 _LAYER_UIDS = {}
+
 
 #Helper function, assigns unique layer IDs
 def get_layer_uid(layer_name=''):
@@ -17,7 +19,7 @@ def get_layer_uid(layer_name=''):
         return _LAYER_UIDS[layer_name]
 
 
-"""Dropout for sparse tensors."""
+#Dropout for sparse tensors
 def sparse_dropout(x, keep_prob, noise_shape):
     random_tensor = keep_prob
     random_tensor += tf.random_uniform(noise_shape)
@@ -36,36 +38,28 @@ def dot(x, y, sparse=False):
 
 
 class Layer(object):
-    """Base layer class. Defines basic API for all layer objects.
-    Implementation inspired by keras (http://keras.io).
-    # Properties
-        name: String, defines the variable scope of the layer.
-        logging: Boolean, switches Tensorflow histogram logging on/off
-    # Methods
-        _call(inputs): Defines computation graph of layer
-            (i.e. takes input, returns output)
-        __call__(inputs): Wrapper for _call()
-        _log_vars(): Log all variables
-    """
 
     def __init__(self, **kwargs):
-        allowed_kwargs = {'name', 'logging'}
-        for kwarg in kwargs.keys():
-            assert kwarg in allowed_kwargs, 'Invalid keyword argument: ' + kwarg
+
         name = kwargs.get('name')
         if not name:
             layer = self.__class__.__name__.lower()
             name = layer + '_' + str(get_layer_uid(layer))
+
         self.name = name
         self.vars = {}
         logging = kwargs.get('logging', False)
         self.logging = logging
         self.sparse_inputs = False
 
+
     def _call(self, inputs):
+
         return inputs
 
+
     def __call__(self, inputs):
+
         with tf.name_scope(self.name):
             if self.logging and not self.sparse_inputs:
                 tf.summary.histogram(self.name + '/inputs', inputs)
@@ -74,60 +68,16 @@ class Layer(object):
                 tf.summary.histogram(self.name + '/outputs', outputs)
             return outputs
 
+
     def _log_vars(self):
+    	
         for var in self.vars:
             tf.summary.histogram(self.name + '/vars/' + var, self.vars[var])
 
 
-class Dense(Layer):
-    """Dense layer."""
-    def __init__(self, input_dim, output_dim, placeholders, dropout=0., sparse_inputs=False,
-                 act=tf.nn.relu, bias=False, featureless=False, **kwargs):
-        super(Dense, self).__init__(**kwargs)
-
-        if dropout:
-            self.dropout = placeholders['dropout']
-        else:
-            self.dropout = 0.
-
-        self.act = act
-        self.sparse_inputs = sparse_inputs
-        self.featureless = featureless
-        self.bias = bias
-
-        # helper variable for sparse dropout
-        self.num_features_nonzero = placeholders['num_features_nonzero']
-
-        with tf.variable_scope(self.name + '_vars'):
-            self.vars['weights'] = glorot([input_dim, output_dim],
-                                          name='weights')
-            if self.bias:
-                self.vars['bias'] = zeros([output_dim], name='bias')
-
-        if self.logging:
-            self._log_vars()
-
-    def _call(self, inputs):
-        x = inputs
-
-        # dropout
-        if self.sparse_inputs:
-            x = sparse_dropout(x, 1-self.dropout, self.num_features_nonzero)
-        else:
-            x = tf.nn.dropout(x, 1-self.dropout)
-
-        # transform
-        output = dot(x, self.vars['weights'], sparse=self.sparse_inputs)
-
-        # bias
-        if self.bias:
-            output += self.vars['bias']
-
-        return self.act(output)
-
-
+#Graph convolution layer
 class GraphConvolution(Layer):
-    """Graph convolution layer."""
+
     def __init__(self, input_dim, output_dim, placeholders, dropout=0.,
                  sparse_inputs=False, act=tf.nn.relu, bias=False,
                  featureless=False, **kwargs):
@@ -156,6 +106,7 @@ class GraphConvolution(Layer):
 
         if self.logging:
             self._log_vars()
+
 
     def _call(self, inputs):
         x = inputs
