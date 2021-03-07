ae05e10df6dd41903783a7848745f2ca98305123
Aniket Agnihotri <aniket360@gmail.com>
2021-03-07 16:41:50 -0500

    map distance function implemented

--- frontend/components/map/MapManager.jsx
+++ frontend/components/map/MapManager.jsx
@@ -1,38 +1,70 @@
-import React, {useState} from "react";
+import React, {useState, useEffect } from "react";
 import {Image, StyleSheet, View, Text, Alert} from "react-native";
 import MapView, { Marker } from 'react-native-maps';
+import getDistance from 'geolib/es/getDistance';
+import convertDistance from 'geolib/es/convertDistance'
 import Logo from "../../resources/logo.png";
-import { Button } from 'native-base';
 
-function MapManager({navigation}) {
-    const [latitude, setLatitude] = useState('');
-    const [longitude, setLongitude] = useState('');
+function MapManager({route, navigation}) {
+    const [fordDistance, setFordDistance] = useState('');
+    const [wileyDistance, setWileyDistance] = useState('');
+    const [windsorDistance, setWindsorDistance] = useState('');
+    const [earhartDistance, setEarhartDistance] = useState('');
+    const [hillenbrandDistance, setHillenbrandDistance] = useState('');
 
-    async function findCoordinates() {
-        await navigator.geolocation.getCurrentPosition(
-            position => {
-                position = JSON.stringify(position);
-                position = JSON.parse(position);
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+        position => {
+            let ford = (getDistance(position.coords, {
+                latitude: 40.43210018,
+                longitude: -86.91955498354119
+            }))
+            ford = convertDistance(ford, 'mi')
+            ford = ford.toFixed(2)
+            setFordDistance(ford);
 
-                // setResponse(location)
-                setLatitude(position["coords"]['latitude']);
-                setLongitude(position["coords"]['longitude']);
+            let windsor = (getDistance(position.coords, {
+                latitude: 40.42655749633789,
+                longitude: -86.9213159984375
+            }))
+            windsor = convertDistance(windsor, 'mi')
+            windsor = windsor.toFixed(2)
+            setWindsorDistance(windsor);
+
+            let wiley = (getDistance(position.coords, {
+                latitude: 40.4285107,
+                longitude: -86.9208281
+            }))
+            wiley = convertDistance(wiley, 'mi')
+            wiley = wiley.toFixed(2)
+            setWileyDistance(wiley);
+
+            let hillenbrand = (getDistance(position.coords, {
+                latitude: 40.42688475,
+                longitude: -86.92629084966707
+            }))
+            hillenbrand = convertDistance(hillenbrand, 'mi')
+            hillenbrand = hillenbrand.toFixed(2)
+            setHillenbrandDistance(hillenbrand);
+
+            let earhart = (getDistance(position.coords, {
+                latitude: 40.425601350000004,
+                longitude: -86.925110299389807
+            }))
+            earhart = convertDistance(earhart, 'mi')
+            earhart = earhart.toFixed(2)
+            setEarhartDistance(earhart);
             },
             error => Alert.alert(error.message),
             { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
         );
-    }
+    });
 
     return (
         <View>
             <View style={ styles.screenView }>
                 <Image style={ styles.logoImage } source={ Logo } />
             </View>
-            <Button onPress={ findCoordinates } >
-                <Text>Find location</Text>
-            </Button>
-            <Text>Latitude: {latitude}</Text>
-            <Text>Longitude: {longitude}</Text>
             <View>
                 <Text style={ styles.screenTitle }> Dining Locations </Text>
             </View>
@@ -52,35 +84,36 @@ function MapManager({navigation}) {
                     coordinate={{latitude: 40.43210018,
                         longitude: -86.91955498354119}}
                     title={"Ford"}
-                    description={"Dine-in/On-to-go available"}
-                />
+                    description={"Dine-in/On-the-go available\n" + fordDistance + " miles away"}
+                >
+                </Marker>
 
                 <Marker
                     coordinate={{latitude: 40.42655749633789,
                         longitude: -86.9213159984375}}
                     title={"Windsor"}
-                    description={"Dine-in/On-to-go available"}
+                    description={"Dine-in/On-the-go  available\n" + windsorDistance + " miles away"}
                 />
 
                 <Marker
                     coordinate={{latitude: 40.4285107,
                         longitude: -86.9208281}}
                     title={"Wiley"}
-                    description={"Dine-in"}
+                    description={"Dine-in\n" + wileyDistance + " miles away"}
                 />
 
                 <Marker
                     coordinate={{latitude: 40.42688475,
                         longitude: -86.92629084966707}}
                     title={"Hillenbrand"}
-                    description={"Dine-in"}
+                    description={"Dine-in\n" + hillenbrandDistance + " miles away"}
                 />
 
                 <Marker
                     coordinate={{latitude: 40.425601350000004,
                         longitude: -86.925110299389807}}
                     title={"Earhart"}
-                    description={"Dine-in/On-to-go available"}
+                    description={"Dine-in/On-the-go available\n" + earhartDistance + " miles away"}
                 />
             </MapView>
         </View>
