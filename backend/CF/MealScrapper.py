fffd41d5f775ba28a7428981a2622e7526dda80e
Vaastav Arora <arora74@purdue.edu>
2021-03-09 21:11:14 -0500

    cleanup

--- backend/CF/MealScrapper.py
+++ backend/CF/MealScrapper.py
@@ -2,8 +2,9 @@ import requests
 from datetime import datetime
 from google.cloud import bigquery
 
+
 client = bigquery.Client()
-table_id = "[PROJECT-ID].[DATASET].[TABLE]"
+table_id = "purdueeats-304919.PurdueEatsDatabase"
 
 
 URL = 'https://api.hfs.purdue.edu/menus/v2/locations/'
@@ -20,16 +21,9 @@ def meal_scrapper(request):
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
-    DATE = datetime.today().strftime('%Y-%m-%d')
+
+    DATE = '/' + datetime.today().strftime('%Y-%m-%d')
 
     for loc in LOCATIONS:
-        pass
-    
-    response = requests.get('')
-    request_json = request.get_json()
-    if request.args and 'message' in request.args:
-        return request.args.get('message')
-    elif request_json and 'message' in request_json:
-        return request_json['message']
-    else:
-        return f'Hello World!'
+        response = requests.get(URL + loc + DATE)
+        print(response)
