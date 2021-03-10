7ec6b0540bd2ccf93d2813815bcff79860938f55
Vaastav Arora <arora74@purdue.edu>
2021-03-10 00:46:53 -0500

    DF menu items implemented

--- backend/CF/MealScrapper.py
+++ backend/CF/MealScrapper.py
@@ -23,32 +23,36 @@ def meal_scrapper(request):
     MAX_ID = [dict(row) for row in runQuery(
         "SELECT max(MenuItemID) FROM MenuItems")]
     
-    if MAX_ID:
+
+    if not MAX_ID[0]['f0_']:
         MAX_ID = 1
     else:
-        MAX_ID[0]['f0_'] + 1
+        MAX_ID = MAX_ID[0]['f0_'] + 1
 
     for loc in LOCATIONS:
 
         response = requests.get(URL + loc + DATE).json()
 
-        #DF_ID = [dict(row) for row in runQuery(
-        #   f"""SELECT DiningFacilityID FROM UserBasic WHERE 
-        #    DiningFacilityName = '{loc}'""")][0]
+        DF_ID = [dict(row) for row in runQuery(
+           f"""SELECT DiningFacilityID FROM DiningFacilities WHERE 
+            DiningFacilityName = '{loc}'""")][0]
 
+        runQuery(
+            f"""DELETE FROM DiningFacilityMenuItems 
+            WHERE DiningFacilityID = {DF_ID['DiningFacilityID']}""")
+        
         for meals in response['Meals']:
             
             time = meals['Hours']['StartTime'] + "-" + meals['Hours']['EndTime']
-            time = ' '.join(format(ord(x), 'b') for x in time)
 
             for station in meals['Stations']:
                 
                 for item in station['Items']:
 
                     find = [dict(row) for row in runQuery(
-                        f"SELECT COUNT(*) FROM MenuItems WHERE HashID = '{item['ID']}'")]
+                        f"SELECT MenuItemID FROM MenuItems WHERE HashID = '{item['ID']}'")]
                     
-                    if find[0]['f0_'] != 1: 
+                    if len(find) != 1: 
 
                         query = 'INSERT INTO MenuItems values ('
                         query += f"{str(MAX_ID)}, '{item['ID']}', \"{item['Name']}\","
@@ -61,13 +65,26 @@ def meal_scrapper(request):
                             query += str(allergen['Value']).lower() + ','
                         
                         query = query[:-1] + ')'
-                        print(query)
+                        
+                        runQuery(query)
+
+                        query = 'INSERT INTO DiningFacilityMenuItems values ('
+                        query += f"{str(DF_ID['DiningFacilityID'])}, {MAX_ID}, "
+                        query += f"b'{time}'" + ','
+                        query += f"\"{station['Name']}\"" + ')'
+
                         runQuery(query)
 
                         MAX_ID += 1
+                    
+                    else:
+                        
+                        query = 'INSERT INTO DiningFacilityMenuItems values ('
+                        query += f"{str(DF_ID['DiningFacilityID'])}, {find[0]['MenuItemID']}, "
+                        query += f"b'{time}'" + ','
+                        query += f"\"{station['Name']}\"" + ')'
 
-        print(response['Location'])
-        print()
+                        runQuery(query)
 
 
 if __name__ == "__main__":
