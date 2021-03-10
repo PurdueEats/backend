c6143098ad4e71cb2d71f839373180fb9329409f
Vaastav Arora <arora74@purdue.edu>
2021-03-09 22:35:14 -0500

    MenuItem loader complete

--- backend/CF/MealScrapper.py
+++ backend/CF/MealScrapper.py
@@ -21,9 +21,14 @@ def meal_scrapper(request):
 
     DATE = '/' + datetime.today().strftime('%Y-%m-%d')
     MAX_ID = [dict(row) for row in runQuery(
-        "SELECT max(MenuItemID) FROM MenuItems")][0]['f0_'] + 1
+        "SELECT max(MenuItemID) FROM MenuItems")]
+    
+    if MAX_ID:
+        MAX_ID = 1
+    else:
+        MAX_ID[0]['f0_'] + 1
 
-    for i, loc in enumerate(LOCATIONS):
+    for loc in LOCATIONS:
 
         response = requests.get(URL + loc + DATE).json()
 
@@ -32,22 +37,35 @@ def meal_scrapper(request):
         #    DiningFacilityName = '{loc}'""")][0]
 
         for meals in response['Meals']:
-            #print(meals)
+            
             time = meals['Hours']['StartTime'] + "-" + meals['Hours']['EndTime']
             time = ' '.join(format(ord(x), 'b') for x in time)
 
             for station in meals['Stations']:
                 
                 for item in station['Items']:
-                    query = 'INSERT INTO MenuItems values ('
-                    query += f"{str(MAX_ID)}, '{item['ID']}', '{item['Name']}',"
-                    query += "b'Nutrition',"
 
-                    for alllergen in item['Allergen']
+                    find = [dict(row) for row in runQuery(
+                        f"SELECT COUNT(*) FROM MenuItems WHERE HashID = '{item['ID']}'")]
+                    
+                    if find[0]['f0_'] != 1: 
 
+                        query = 'INSERT INTO MenuItems values ('
+                        query += f"{str(MAX_ID)}, '{item['ID']}', \"{item['Name']}\","
+                        query += "b'Nutrition',"
 
+                        if 'Allergens' not in item:
+                            item['Allergens'] = [{'Value': False} for _ in range(11)]
+                    
+                        for allergen in item['Allergens']:
+                            query += str(allergen['Value']).lower() + ','
+                        
+                        query = query[:-1] + ')'
+                        print(query)
+                        runQuery(query)
 
-            pass
+                        MAX_ID += 1
+
         print(response['Location'])
         print()
 
