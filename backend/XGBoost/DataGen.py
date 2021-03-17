6b75c0b86db2ce7fc6e9d5811cba28fdf5a850ce
Vaastav Arora <arora74@purdue.edu>
2021-03-16 22:33:50 -0400

    Data Generator added

---
+++ backend/XGBoost/DataGen.py
@@ -0,0 +1,52 @@
+import requests
+from datetime import date, timedelta
+
+URL = 'https://api.hfs.purdue.edu/menus/v2/locations/'
+LOCATIONS = ['Earhart', 'Hillenbrand', 'Ford', 'Windsor', 'Wiley']
+DATE = ''
+
+# Change based on local file system
+FILENAME = 'temp.csv'
+
+
+START_DATE = date(2016, 8, 22)
+END_DATE = date(2021, 3, 1)
+
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+
+def generate_dataset():
+
+    file = open(FILENAME, 'a')
+
+    for single_date in daterange(START_DATE, END_DATE):
+
+        cur_date = single_date.strftime("%Y-%m-%d")
+
+        for loc in LOCATIONS:
+
+            response = requests.get(URL +'/' + loc + '/' + cur_date).json()
+
+            meals_list = []
+
+            try:
+                meals_list = [x for x in response['Meals'] if x['Status'] ==  'Open']
+            except: 
+                pass
+
+            for meals in meals_list:
+
+                for station in meals['Stations']:
+                    
+                    for item in station['Items']:
+
+                        file.write(cur_date + ';' + loc + ';' + item['Name'] + '\n')
+
+    file.close()
+
+
+if __name__ == "__main__":
+    generate_dataset()
