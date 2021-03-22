b3fb9d31f646bbc125db8df6fbfa9a74a38d37db
Vaastav Arora <arora74@purdue.edu>
2021-03-21 23:45:52 -0400

    User Nutrition Cleaner added

---
+++ backend/CF/UserNutritionCleaner.py
@@ -0,0 +1,32 @@
+from google.cloud import bigquery
+
+
+gcp_project = "purdueeats-304919"
+bq_dataset = "PurdueEatsDatabase"
+
+client = bigquery.Client(project=gcp_project)
+dataset_ref = client.dataset(bq_dataset)
+
+
+def runQuery(sql: str):
+    job_config = bigquery.QueryJobConfig()
+    job_config.default_dataset = dataset_ref
+    query = client.query(sql, job_config)
+    results = query.result()
+    return results
+
+
+def nutrition_cleaner(request):
+
+    runQuery("DELETE FROM UserNutrition")
+
+    res = [dict(row) for row in runQuery("SELECT UserID FROM UserBasic")]
+
+    for user_id in res:
+
+        runQuery(f"""
+        INSERT INTO UserNutrition values
+        ({user_id['UserID']}, 0, 0, 0, 0)
+        """)
+    
+    return
