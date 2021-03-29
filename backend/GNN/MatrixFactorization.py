186dab31b658c35c81fd20b4eab876ff51555fbc
Vaastav Arora <arora74@purdue.edu>
2021-03-29 09:38:14 -0400

    factorization logic added

---
+++ backend/GNN/MatrixFactorization.py
@@ -0,0 +1,50 @@
+import numpy
+
+def matrix_factorization(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02):
+    '''
+    R: rating matrix
+    P: |U| * K (User features matrix)
+    Q: |D| * K (Item features matrix)
+    K: latent features
+    steps: iterations
+    alpha: learning rate
+    beta: regularization parameter'''
+    Q = Q.T
+
+    for step in range(steps):
+
+        for i in range(len(R)):
+
+            for j in range(len(R[i])):
+
+                if R[i][j] > 0:
+                    
+                    # calculate error
+                    eij = R[i][j] - numpy.dot(P[i,:],Q[:,j])
+
+                    for k in range(K):
+                        # calculate gradient with a and beta parameter
+                        P[i][k] = P[i][k] + alpha * (2 * eij * Q[k][j] - beta * P[i][k])
+                        Q[k][j] = Q[k][j] + alpha * (2 * eij * P[i][k] - beta * Q[k][j])
+
+        eR = numpy.dot(P,Q)
+
+        e = 0
+
+        for i in range(len(R)):
+
+            for j in range(len(R[i])):
+
+                if R[i][j] > 0:
+
+                    e = e + pow(R[i][j] - numpy.dot(P[i,:],Q[:,j]), 2)
+
+                    for k in range(K):
+
+                        e = e + (beta/2) * (pow(P[i][k],2) + pow(Q[k][j],2))
+        # 0.001: local minimum
+        if e < 0.001:
+
+            break
+
+    return P, Q.T
