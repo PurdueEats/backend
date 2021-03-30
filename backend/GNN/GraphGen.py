7bfdbca4b2d30391316e62e742b8b24e6ad7dcb5
Vaastav Arora <arora74@purdue.edu>
2021-03-30 05:06:08 -0400

    Datafram to numpy conversion added

--- backend/GNN/GraphGen.py
+++ backend/GNN/GraphGen.py
@@ -17,6 +17,16 @@ def graph_gen():
     reviews = pd.read_csv("temp/clean_reviews.csv")
     print(reviews.head)
 
+    src = np.zeros(reviews.shape[0], dtype=np.int64)
+    dst = np.zeros(reviews.shape[0], dtype=np.int64)
+    rating = np.zeros((reviews.shape[0], 5), dtype=np.int64)
+
+    for index, row in reviews.iterrows():
+
+        src[index] = int(row['RecipeID'])
+        dst[index] = int(row['profileID'])
+        rating[index][int(row['Rate'])-1] = 1
+
 
 if __name__ == "__main__":
     graph_gen()
