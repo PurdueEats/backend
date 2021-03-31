72d3b052c947c74c8e04e96dace80c065f405389
Vaastav Arora <arora74@purdue.edu>
2021-03-31 17:15:22 -0400

    DF get all test implemented

--- backend/API/tests/test_dining_facilities.py
+++ backend/API/tests/test_dining_facilities.py
@@ -6,7 +6,22 @@ client = TestClient(router)
 
 
 def test_get_dining_facilities():
-    pass
+    
+    response = client.get("DF/")
+
+    assert response.status_code == 200
+
+    facilities = set('Earhart', 'Hillenbrand', 'Ford', 'Wiley', 'Windsor')
+    for facility in response.json():
+        
+        assert 'dining_facility_id'     in facility
+        assert 'dining_facility_name'   in facility
+        assert'description'             in facility
+        assert'address'                 in facility
+        assert'image'                   in facility
+    
+        assert facility['dining_facility_name'] in facilities
+        facilities.remove(facility['dining_facility_name'])
 
 
 def test_get_specific__dining_facility():
