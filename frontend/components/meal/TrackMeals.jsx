89f7d76b11a7c41d7cc3c2c95c6c327be0650e62
thomp721 <thomp721@purdue.edu>
2021-04-01 16:39:24 -0400

    Trackmeals is functional, (sort could use a bit of work and looks kinda ugly, but it is functional)

--- frontend/components/meal/TrackMeals.jsx
+++ frontend/components/meal/TrackMeals.jsx
@@ -13,7 +13,19 @@ function TrackMeals({route, navigation}) {
      const [currentSelectID, setCurrentSelectID] = React.useState([]);
      const [currentSelection, setCurrentSelection] = React.useState([]);
      const [removeSelection, setRemoveSelection] = React.useState([]);
-
+     const [label, setLabel] = useState('');
+     const [valueTest, setValueTest] = useState('');
+     const [bool, setBool] = useState('a');
+     const EmptyListMessage = ({item}) => {
+        return (
+          // Flat List Item
+          <Text
+            style={styles.emptyListStyle}
+            onPress={() => getItem(item)}>
+            No Data Found
+          </Text>
+        );
+      };
     /*
      const meals = [
          { label: 'Bangkok Chicken Wrap', value: 21 },
@@ -29,17 +41,55 @@ function TrackMeals({route, navigation}) {
 
      useEffect(() => {
           //testList();
-          getMealIds();
-          getMealInfo();
-          sortList();
+          //setCurrentSelectID([]);
+          //getMealIds();
+          //sortList();
+
+          getMealIds2();
+
      },[]);
 
     function sortList() {
-         currentSelection.sort((a, b) => { return a.timestamp - b.timestamp; });
+         currentSelection.sort(function(a, b) { return a.timestamp < b.timestamp; });
 
+         setBool('a');
+         setBool('b');
     }
 
     function getMealIds() {
+       fetch(`https://purdueeats-304919.uc.r.appspot.com/MenuItemReview/`  + route.params.UserID, {
+            method: 'GET',
+            headers : {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' +  route.params.token
+            },
+        })
+        .then(
+            function(response) {
+                if (response.status === 200 || response.status === 201) {
+                    console.log("fetch1 worked");
+                    // Successful GET
+                    // Set fields to correct values
+                    response.json().then(function(data) {                            //currentSelectID.push(item.menu_item_id);
+                            setCurrentSelectID(data.map(menuItem => ({ value: menuItem.menu_item_id, timestamp: menuItem.timestamp })));
+
+                    });
+                    //console.log(currentSelection);
+
+                } else {
+                    console.log('Auth like there was a problem with ID fetching. Status Code: ' +
+                        response.status);
+                }
+            }
+        )
+        .catch(function(err) {
+            console.log('Fetch Error :-S', err)
+        });
+
+    }
+
+    function getMealIds2() {
 
        fetch(`https://purdueeats-304919.uc.r.appspot.com/MenuItemReview/`  + route.params.UserID, {
             method: 'GET',
@@ -52,27 +102,63 @@ function TrackMeals({route, navigation}) {
         .then(
             function(response) {
                 if (response.status === 200 || response.status === 201) {
-                    console.log("fetch1 worked")
                     // Successful GET
                     // Set fields to correct values
-                    response.json().then(function(data) {                            //currentSelectID.push(item.menu_item_id);
-                            setCurrentSelectID(data.map(menuItem => ({ value: menuItem.menu_item_id, timestamp: menuItem.timestamp })));
-                    });
 
-                    //console.log(currentSelection);
-                    console.log("fetch1 worked5645")
+                    /* response.json().then(function(data) {                            //currentSelectID.push(item.menu_item_id);
+                        setCurrentSelectID(data.map(menuItem => ({ value: menuItem.menu_item_id, timestamp: menuItem.timestamp })));
 
+                    }); */
+                    //console.log(currentSelectID);
+                    return response.json();
                 } else {
                     console.log('Auth like there was a problem with ID fetching. Status Code: ' +
                         response.status);
                 }
             }
-        )
+        ).then(item => {
+            item.map(item1 => {
+            fetch(`https://purdueeats-304919.uc.r.appspot.com/MenuItems/` + item1.menu_item_id, {
+                method: 'GET',
+                headers : {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                },
+            })
+                .then(
+                    function(response) {
+                        if (response.status === 200 || response.status === 201) {
+                            // Successful GET
+                            // Set Fields to correct values
+                             response.json().then(function(data) {
+                                currentSelection.push( { label: data.item_name, value: item1.menu_item_id, timestamp: item1.timestamp } );
+                                //setCurrentSelection( ({ label: data.item_name, value: item.id, timestamp: item.timestamp } ));
+                            });
+
+                            setBool('b');
+                            //sortList();
+                            setBool('a');
+                        } else {
+                            console.log('Getting Menu Item` like there was a problem. Status Code: ' +
+                                response.status);
+                        }
+                    }
+                )
+                .catch(function(err) {
+                    console.log('Fetch sdfError :-S', err)
+                });
+            })
+
         .catch(function(err) {
-            console.log('Fetch Error :-S', err);
+            console.log('Fetch Error :-S', err)
         });
 
-    }
+    }).catch(function(err) {
+        console.log("err");
+      });
+   }
+
+
 
     function testList() {
         //setCurrentSelectID(meals.map(item => {item.value}));
@@ -85,9 +171,6 @@ function TrackMeals({route, navigation}) {
 
     // GET request to convert selected menu item(s) ID(s) to the respective name(s)
      function getMealInfo() {
-        console.log("getFavMealBegiing");
-        console.log("hereeeeee")
-        console.log(currentSelection);
        currentSelectID.map(item => {
          fetch(`https://purdueeats-304919.uc.r.appspot.com/MenuItems/` + item.value, {
                 method: 'GET',
@@ -107,7 +190,7 @@ function TrackMeals({route, navigation}) {
                             });
                             console.log(currentSelection)
                         } else {
-                            console.log('Getting Menu Items like there was a problem. Status Code: ' +
+                            console.log('Getting Menu Item` like there was a problem. Status Code: ' +
                                 response.status);
                         }
                     }
@@ -117,18 +200,49 @@ function TrackMeals({route, navigation}) {
                 });
             })
         }
+
+        function getMealInfo2(value) {
+             fetch(`https://purdueeats-304919.uc.r.appspot.com/MenuItems/` + value, {
+                    method: 'GET',
+                    headers : {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json',
+                    },
+                })
+                    .then(
+                        function(response) {
+                            if (response.status === 200 || response.status === 201) {
+                                console.log("got to here")
+                                // Successful GET
+                                // Set Fields to correct values
+                                 response.json().then(function(data) {
+                                    setLabel(data.item_name);
+                                    //setCurrentSelection( ({ label: data.item_name, value: item.id, timestamp: item.timestamp } ));
+                                });
+                            } else {
+                                console.log('Getting Menu Items like there was a problem. Status Code: ' +
+                                    response.status);
+                            }
+                        }
+                    )
+                    .catch(function(err) {
+                        console.log('Fetch sdfError :-S', err)
+                    });
+
+            }
     const Item = ({ label }) => (
       <View style={styles.item}>
         <Text style={styles.label}>{label}</Text>
       </View>
     );
-    const renderItem = ({ item }) => (
+    function renderItem(item) {
+    return (
     <TouchableOpacity onPress={() =>  navigation.navigate("MealNutrition", { UserID: route.params.UserID, token: route.params.token,
-                                                                                    MealName: item.label,
-                                                                                     MealID: item.value }) }>
+                                                                                    MealName: item.item.label,
+                                                                                    MealID: item.item.value }) }>
         <View style={{flexDirection:"row"}}>
             <View style={{alignItems: 'flex-end'}}>
-                <Item label={item.timestamp} />
+                <Item label={ item.item.label + " " + item.item.timestamp } />
             </View>
             <View style={{position: 'absolute', right: 10, bottom: 15}}>
                 <MaterialCommunityIcons name="arrow-right" color="red" size={30}/>
@@ -141,8 +255,9 @@ function TrackMeals({route, navigation}) {
             </View>
         </View>
         </TouchableOpacity>
+        );
+    }
 
-    );
     return (
         // Do not remove ScrollView. Adds scrolling to screens.
         <ScrollView showsVerticalScrollIndicator={false}>
@@ -154,15 +269,26 @@ function TrackMeals({route, navigation}) {
             </View>
             <View>
                 <Text style={ styles.screenTitle }>Track Meals</Text>
+                <TouchableOpacity style={ styles.sortTitle } onPress={ () => sortList()}>
+                    <Text style={ styles.sortTitle }>Sort</Text>
+                </TouchableOpacity>
             </View>
+
+            <View style={ [styles.screenView, {flexDirection:"row"}] } >
+             </View>
              <View>
                   <SafeAreaView style={styles.container}>
                     <FlatList
                       data={ currentSelection }
-                      renderItem={ renderItem }
+                      renderItem={ (item) => renderItem(item) }
+                      extraData={ bool.state }
+                      keyExtractor={item => { Math.random().toString(36).substring(5) } }
+                      ListEmptyComponent={ EmptyListMessage }
+
                     />
                   </SafeAreaView>
              </View>
+
             <View style={ [styles.buttonView, {alignItems:"center"}] }>
             <View style={ styles.notView }>
             </View>
@@ -198,6 +324,13 @@ const styles = StyleSheet.create({
     },
     screenTitle: {
         fontSize: 26,
+        fontWeight: "bold",
+        textAlign: "center",
+        marginBottom: "2%",
+    },
+
+     sortTitle: {
+        fontSize: 17,
         fontWeight: "bold",
         textAlign: "center",
         marginBottom: "2%",
