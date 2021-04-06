72644b263334c4784f5f9ed5ae2d306b587894e0
seanjoo <joo21@purdue.edu>
2021-04-06 02:00:09 -0400

    made dark mode compatible with Android devices

--- frontend/android/app/src/main/java/com/purdueeats/MainActivity.java
+++ frontend/android/app/src/main/java/com/purdueeats/MainActivity.java
@@ -2,7 +2,19 @@ package com.purdueeats;
 
 import com.facebook.react.ReactActivity;
 
+import android.content.Intent;
+
+import android.content.res.Configuration;
+
 public class MainActivity extends ReactActivity {
+
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    Intent intent = new Intent("onConfigurationChanged");
+    intent.putExtra("newConfig", newConfig);
+    sendBroadcast(intent);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
