5dea108b613dd42f6761f59ffe000fb2ebf433b5
anishasnet <anishasnet@gmail.com>
2021-04-11 20:56:12 -0400

    write reviews complete

--- frontend/components/dining/WriteReview.jsx
+++ frontend/components/dining/WriteReview.jsx
@@ -4,12 +4,14 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useTheme } from '@react-navigation/native';
 import {Button, Item, Toast} from 'native-base';
 import { StackActions } from '@react-navigation/native';
+import { AirbnbRating } from 'react-native-ratings';
+
 
 function WriteReview({route, navigation}) {
     const { colors } = useTheme();
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
-
+    const [diningRating, setDiningRating] = useState(3);
     useEffect(() => {
 
     }, []);
@@ -20,10 +22,53 @@ function WriteReview({route, navigation}) {
             reviewError();
             return;
         }
-        else {
-            reviewSubmitted()
-            navigation.navigate("ReadReviews", { UserID: route.params.UserID, token: route.params.token, DiningID: route.params.DiningID });
-        }
+        fetch(`https://app-5fyldqenma-uc.a.run.app/DFR/`, {
+            method: 'POST',
+            headers : {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify( {
+                "dining_facility_review_id": "",
+                "user_id": route.params.UserID,
+                "dining_facility_id": route.params.DiningID,
+                "title": title,
+                "review_text": body,
+                "rating": diningRating,
+                "upvote_count": 0,
+                "downvote_count": 0
+                }
+            )
+        })
+            .then(
+                function(response) {
+                    if (response.status === 200 || response.status === 201) {
+                        // Successful POST
+                        reviewSubmitted()
+                        navigation.navigate("ReadReviews", { UserID: route.params.UserID, token: route.params.token, DiningID: route.params.DiningID });
+                    } else {
+                        // Examine the text in the response
+                        console.log('Looks like there was a problem submitting the review. Status Code: ' +
+                            response.status);
+                        displayError();
+                    }
+                }
+            )
+            .catch(function(err) {
+                console.log('Fetch Error :-S', err);
+            });
+    }
+
+    function displayError() {
+        Toast.show({
+            style: { backgroundColor: "red", justifyContent: "center" },
+            position: "top",
+            text: "The dining court review was not submitted. Please try again.",
+            textStyle: {
+                textAlign: 'center',
+            },
+            duration: 1500
+        });
     }
 
     function reviewSubmitted() {
@@ -49,7 +94,9 @@ function WriteReview({route, navigation}) {
             duration: 1500
         });
     }
-
+    function handleRating(rating) {
+        setDiningRating(rating);
+    }
     return (
         <ScrollView>
             <View style={ styles.topView } >
@@ -58,6 +105,17 @@ function WriteReview({route, navigation}) {
                 </TouchableOpacity>
                 <Text style={ [styles.screenTitle, {color: colors.text}] }>Write a Review</Text>
             </View>
+            <AirbnbRating
+                count={5}
+                reviews={["Terrible", "Meh", "OK", "Good", "Amazing"]}
+                type={"custom"}
+                showRating={false}
+                selectedColor={"#ff0000"}
+                defaultRating={3}
+                reviewSize={20}
+                size={25}
+                onFinishRating={handleRating}
+            />
             <Text style={ [styles.subheader, {color: colors.text}] }>Title</Text>
             <Item style={ styles.titleInput }>
                 <TextInput style={ [styles.textInput, {color: colors.text}] } placeholder="Enter your review title here" onChangeText={(title) => setTitle(title)} />
