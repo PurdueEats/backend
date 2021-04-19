e900856b1de20f9f66791f90cd85f5394d15361a
Aniket Agnihotri <aniket360@gmail.com>
2021-04-19 17:41:44 -0400

    merged w main

--- frontend/App.js
+++ frontend/App.js
@@ -4,31 +4,34 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { AppearanceProvider, useColorScheme } from "react-native-appearance";
 import { DefaultTheme, DarkTheme } from "@react-navigation/native";
+import NavBarManager from "./components/navbar/NavBarManager";
 import LoginManager from "./components/login/LoginManager";
-import ProfileManager from "./components/profile/ProfileManager";
+import Name from "./components/login/signup/Name";
+import Email from "./components/login/signup/Email";
+import Password from "./components/login/signup/Password";
+import MealPlan from "./components/login/signup/MealPlan";
 import MealPreferences from "./components/login/login-accessories/MealPreferences";
 import DiningCourt from "./components/home/HomeManager";
 import Menu from "./components/home/menu/Menu";
 import MealNutrition from "./components/home/menu/MealNutrition";
 import RecordMeals from "./components/home/menu/RecordMeals";
-import Name from "./components/login/signup/Name";
-import Email from "./components/login/signup/Email";
-import Password from "./components/login/signup/Password";
-import MealPlan from "./components/login/signup/MealPlan";
 import MapManager from "./components/map/MapManager";
+import ProfileManager from "./components/profile/ProfileManager";
+import SemesterSummary from "./components/profile/profile-accessories/SemesterSummary";
+import TrackMeals from "./components/profile/profile-accessories/TrackMeals";
+import Notifications from "./components/profile/profile-accessories/Notifications";
+import FavoriteMeal from "./components/profile/profile-accessories/FavoriteMeal";
+import ForgotPassword from "./components/login/login-accessories/ForgotPassword";
+import Schedule from "./components/profile/profile-accessories/Schedule";
 import SettingsManager from "./components/settings/SettingsManager";
 import Feedback from "./components/settings/settings-accessories/Feedback";
-import NavBarManager from "./components/navbar/NavBarManager";
-import ForgotPassword from "./components/login/login-accessories/ForgotPassword";
-import TrackMeals from "./components/profile/profile-accessories/TrackMeals";
-import FavoriteMeal from "./components/profile/profile-accessories/FavoriteMeal";
-import Schedule from "./components/profile/profile-accessories/Schedule";
-import Notifications from "./components/profile/profile-accessories/Notifications";
 import ReadReviews from "./components/home/menu/ReadReviews";
 import WriteReview from "./components/home/menu/WriteReview";
+
 export default function App() {
     const Stack = createStackNavigator();
     const scheme = useColorScheme();
+
     return (
         // Do not remove Root! Root is necessary for toasts integrated in successive components.
         <Root>
@@ -50,12 +53,14 @@ export default function App() {
                         <Stack.Screen name="MealReview" component={ RecordMeals }/>
                         <Stack.Screen name="Map" component={ MapManager }/>
                         <Stack.Screen name="Profile" component={ ProfileManager }/>
+                        <Stack.Screen name="SemesterSummary" component={ SemesterSummary }/>
                         <Stack.Screen name="Settings" component={ SettingsManager }/>
                         <Stack.Screen name="Feedback" component={ Feedback } options={{gestureEnabled: false}}/>
                         <Stack.Screen name="EditSchedule" component={ Schedule }/>
                         <Stack.Screen name="Track" component={ TrackMeals }/>
                         <Stack.Screen name="FavoriteMeal" component= { FavoriteMeal }/>
                         <Stack.Screen name="Notifications" component= { Notifications }/>
+                        <Stack.Screen name="EditSchedule" component={ Schedule }/>
                         <Stack.Screen name="ReadReviews" component= { ReadReviews }/>
                         <Stack.Screen name="WriteReview" component= { WriteReview }/>
                         <Stack.Screen name="NavBar" component={ NavBarManager } options={{gestureEnabled: false}}/>
