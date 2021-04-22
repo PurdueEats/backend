c39a33f0993f5544ea620bb872a7067810d9728e
Vaastav Arora <arora74@purdue.edu>
2021-04-22 13:41:26 -0400

    weeklly summary tests complete

--- backend/API/tests/test_weekly_summary.py
+++ backend/API/tests/test_weekly_summary.py
@@ -5,6 +5,25 @@ from backend.API.routes.api import router
 client = TestClient(router)
 
 
-def test_weekly_summary():
+def test_weekly_summary_1():
 
-    pass
+    response = client.get("/WeeklyNutrition/2021-04-19")
+
+    assert response.status_code == 200
+
+    assert 'calories'   in response.json()
+    assert 'carbs'      in response.json()
+    assert 'fat'        in response.json()
+    assert 'protein'    in response.json()
+
+
+def test_weekly_summary_2():
+
+    response = client.get("/WeeklyNutrition/2021-04-15")
+
+    assert response.status_code == 200
+
+    assert 'calories'   in response.json()
+    assert 'carbs'      in response.json()
+    assert 'fat'        in response.json()
+    assert 'protein'    in response.json()
