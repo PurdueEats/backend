d4655c908421ff27945d8797d4d70ce7e6dc48b6
Vaastav Arora <arora74@purdue.edu>
2021-04-22 13:13:26 -0400

    login tests complete

--- backend/API/tests/test_login.py
+++ backend/API/tests/test_login.py
@@ -1,2 +1,35 @@
 from fastapi.testclient import TestClient
 from backend.API.routes.api import router
+
+
+client = TestClient(router)
+
+
+def test_users_route():
+
+    response = client.get("Users/")
+
+    assert response.status_code == 200
+
+    for user in response.json():
+
+        assert 'user_id'    in user
+        assert 'name'       in user
+        assert 'email'      in user
+
+
+def test_login_route():
+
+    response = client.post(
+        "Users/Login",
+        json={
+            'user_id': 0,
+            'name' : "",
+            'email': 'mark@mark.com',
+            'password': 'string'
+        })
+    
+    assert response.status_code == 200
+
+    assert 'UserID' in response.json()
+    assert 'token'  in response.json()
--- backend/API/tests/test_recommendations.py
+++ backend/API/tests/test_recommendations.py
@@ -1,2 +1,10 @@
 from fastapi.testclient import TestClient
 from backend.API.routes.api import router
+
+
+client = TestClient(router)
+
+
+def test_recommendations():
+
+    pass
